b4947652fb6d9a6722f062dcb4d69d6e220ff0a1
SYNO® <gouthamvijainaduvalath@gmail.com>
2026-05-17 13:37:01 +0400

    chore: migrate from Yarn to pnpm, update CI workflows, and refactor AppComp component

--- web/src/components/AppComp.tsx
+++ web/src/components/AppComp.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Icon } from '@iconify/react';
-import { ActionIcon, Box, Button, Center, Flex, Group, Text } from '@mantine/core';
+import { ActionIcon, Box, Button, Center, Flex, Group, Paper, Text } from '@mantine/core';
 import { useLocales } from '../providers/LocaleProvider';
 import { fetchNui } from '../utils/fetchNui';
 import classes from './App.module.css';
@@ -32,28 +32,23 @@ export default function AppComp() {
   return (
     <Box className={classes.app_container} py={10} h={360} w={330} bg={'#25262b'} mx="auto">
       <Group justify="end" px={8}>
-        <ActionIcon color="yellow" size="lg" radius="md" variant="light" onClick={handleClose}>
+        <ActionIcon color="yellow" size="lg" radius="md" onClick={handleClose}>
           {closeIcon}
         </ActionIcon>
       </Group>
       <Center className={classes.center}>
         <Flex gap="md" justify="center" align="center" direction="column" wrap="wrap">
-          <Button mt={50} variant="light" radius="xl" size="xl" onClick={handleGetPlayerMoney}>
+          <Button mt={50} radius="xl" size="xl" onClick={handleGetPlayerMoney}>
             {locale.ui_buttonText}
           </Button>
-          <Text ta="center" c="white" size="xl" px={6} bg={'#4a4a4a'} fw={700} fz="xl">
-            {locale.ui_playerMoney}
-            <br></br>:{''}
-            {money}
-          </Text>
-          <Button
-            variant="light"
-            color="cyan"
-            radius="md"
-            size="md"
-            tt={'uppercase'}
-            onClick={handleReset}
-          >
+          <Paper withBorder p={10} radius="lg" bg={'#4a4a4a'}>
+            <Text ta="center" c="white" size="xl" px={6} fw={700} fz="xl">
+              {locale.ui_playerMoney}
+              <br></br>:{''}
+              {money}
+            </Text>
+          </Paper>
+          <Button color="cyan" radius="md" size="md" tt={'uppercase'} onClick={handleReset}>
             {locale.ui_reset}
           </Button>
         </Flex>
